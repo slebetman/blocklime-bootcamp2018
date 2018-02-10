28ea11fb8f4f3f9975d8b8507c675e8b568788f5
Adly Abdullah <slebetman@gmail.com>
2018-02-10 15:49:27 +0800

    New code

---
+++ 16-ASSIGNMENT/MyICO.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.18;
+
+import "./FixedSupplyToken.sol";
+
+contract MyICO is SafeMath {
+	FixedSupplyToken token; // initialize tokens
+	
+	
+	
+	function() payable public { // send tokens to people when they send ETH
+	
+	}
+}
