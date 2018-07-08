41fa2ef390076ec74a48cda4c57c23673e1e2f56
Adly Abdullah <slebetman@gmail.com>
2018-07-08 10:35:09 +0800

    Updated examples

--- 07-function_input_output/InputAndOutputParameters.sol
+++ 07-function_input_output/InputAndOutputParameters.sol
@@ -14,17 +14,20 @@ contract InputAndOutputParameters {
     // Define consutruct here
     uint input1;
     address input2;
-    function InputAndOutputParameters(uint _inputParam1, address _inputParam2) public {
+    function InputAndOutputParameters(uint _inputParam1,
+	 address _inputParam2) public {
        // Initialize state variables here
        input1 = _inputParam1;
        input2 = _inputParam2;
     }
 
-    function newFunction1(uint _inputParam1) public pure returns (uint _outputParam1) {
+    function newFunction1(uint _inputParam1)
+		 public pure returns (uint _outputParam1) {
         _outputParam1 = _inputParam1 * 3;
     }
 
-    function newFunction2(uint _inputParam1, uint _inputParam2) public pure returns (uint sum, uint product) {
+    function newFunction2(uint _inputParam1, uint _inputParam2)
+		 public pure returns (uint sum, uint product) {
         sum = _inputParam1 + _inputParam2;
         product = _inputParam1 * _inputParam2;
     }
