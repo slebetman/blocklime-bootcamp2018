0e1329f0a45e0cf8c34f3c39dd1576f687b85a3a
Adly Abdullah <slebetman@gmail.com>
2018-07-08 10:46:04 +0800

    Added delegatecall example

---
+++ 16-delegate_call/delegate_call.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.10;
+
+/*
+ * @title Exceptions Example
+ * @author Harpreet Maan
+ * This is dev for Blocklime's Bootcamp only
+ * Use for Education purpose only
+ *
+ * copyright 2018 Blocklime Technologies Sdn. Bhd.
+ */
+
+contract D {
+  uint public n;
+  address public sender;
+
+  function delegatecallSetN(address _e, uint _n) {
+	_e.delegatecall(bytes4(sha3("setN(uint256)")), _n); // D's storage is set, E is not modified 
+  }
+}
+
+contract E {
+  uint public n;
+  address public sender;
+  function setN(uint _n) {
+	n = _n;
+	sender = msg.sender; // sender is D's caller, not D
+  }
+}
+
